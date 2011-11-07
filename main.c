6065d6ec0fbc5694e9cf98a9b55cf160523c2ffb
Arthur Guimarães <warthur2@gmail.com>
2011-11-07 14:07:07 -0200

    Estruturando o main

--- main.c
+++ main.c
@@ -3,23 +3,35 @@
 #include <cvaux.h>
 #include <highgui.h>
 
+#define max_char = 100
+
 void print_menu();
-int get_input(int tipo);
 void realize_action(int in);
+int get_input_int();
+int get_input_char();
+void realize_action();
+void create_vector();
 
 
 int main(){
 
-  while (input != -1){
+  int input;
+
+  while (input != 3){
     print_menu();
     input = get_input_int();
     realize_action(input);
   }
 
+  return 0;
 }
 
 //Imprime opções de seleção do menu
 void print_menu(){
+  printf("Escolha uma das seguintes opções:\n 
+          1. Inserir conjunto de treino.\n 
+          2. Reconhecer espécie vegetal.\n
+          3. Sair.\n");
 }
 
 //Le informação do usuário:Integer
@@ -31,7 +43,7 @@ int get_input_int(){
 
 //Le informação do usuário:String
 char get_input_char(){
-    char input[100];
+    char input[max_char];
     scanf("%s",&input);
     return input;
 }
@@ -39,19 +51,39 @@ char get_input_char(){
 
 //Faz a chamada para a opção escolhida
 void realize_action(int in){
+  switch (in){
+         case 1:
+              printf("Em qual diretório estão as imagens?\n");
+              char dir[max_char] = get_input_char();
+              create_vector(dir);
+              break;
+         case 2:
+              printf("Qual o endereço da imagem que deseja reconheceer?\n");
+              char file[max_char] = get_input_char();
+              recognize(file); 
+              break;
+         case 3:
+              printf("Tchau!\n")
+              break;
+         default:
+              printf("Este não é um comando válido!\n")
+              break;
+     }
 }
 
+//Faz o reconhecimento da imagem a partir do vetor de características
+void recognize(){
+}
+
+// Cria o vetor de características
 void create_vector(){
     //nome do diretorio e vetor para nomes das imagens
-    char dir[255], file[255];
+    char dir[max_char], file[max_char];
     //Auxiliars
     int i;
     
     printf("Qual o diretório das imagens de treino?")
     scanf("%s",&dir);
-    
-
-
 }
 
 
