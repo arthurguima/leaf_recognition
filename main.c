38d038b3a3d84883bb29468098e9748980dbfeda
Arthur Guimarães <warthur2@gmail.com>
2011-11-07 15:52:41 -0200

    Continuando main

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <string.h> 
 #include <cv.h>
 #include <cvaux.h>
 #include <dirent.h>
 #include <highgui.h>
 
 #define max_char 100
+#define max_species 30
+
+//Estrutura para guardar as características e nome de cada espécie vegetal
+struct{
+  int carac[5];
+  char name[30];
+} carcteristic_vector
+
 
 /*** Assinatura dos métodos ***/
 void  print_menu();
@@ -13,9 +22,10 @@ int   get_input_int();
 char  get_input_char();
 void  create_vector(char *dir);
 void  recognize(char *file);
+void  add_image_to_vector(char *image);
 /*****************************/
 
-
+//Função principal
 int main(){
 
   int input;
@@ -31,10 +41,10 @@ int main(){
 
 //Imprime opções de seleção do menu
 void print_menu(){
-  printf("Escolha uma das seguintes opções:\n");
-  printf("1. Inserir conjunto de treino.\n"); 
-  printf(" 2. Reconhecer espécie vegetal.\n"); 
-  printf(" 3. Sair.\n");
+  puts("Escolha uma das seguintes opções:\n");
+  puts("1. Inserir conjunto de treino.\n"); 
+  puts(" 2. Reconhecer espécie vegetal.\n"); 
+  puts(" 3. Sair.\n");
 }
 
 //Le informação do usuário:Integer
@@ -92,14 +102,18 @@ void create_vector(char *dir){
       perror("Não existem arquivos no diretório");
     else {
       while(file_num--) {
-        printf("Cadastra -> %s\n", namelist[file_num]->d_name);
-        free(namelist[file_num]);
+        //printf("Cadastra -> %s\n", namelist[file_num]->d_name); // !debug
+        add_image_to_vector(namelist[file_num]->d_name); //Usa a imagem como entrada para o vetor
+        free(namelist[file_num]); //libera posição na memória
       }
     }
 }
 
 
+//Adiciona caracteristicas da folha ao vetor de carcterísticas
+void add_image_to_vector(char *image){
 
+}
 
 
 
